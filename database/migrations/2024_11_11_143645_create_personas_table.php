937d3af763bd6b7edbb8a2a4b5d0895c6411e9a7
daparicio85 <43053643@idexperujapon.edu.pe>
2024-11-11 10:24:39 -0500

    cambios solicitados en la migracion

--- database/migrations/2024_11_11_143645_create_personas_table.php
+++ database/migrations/2024_11_11_143645_create_personas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('personas', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
+            $table->string('apellido');
             $table->string('dni')->unique();
             $table->timestamps();
         });
